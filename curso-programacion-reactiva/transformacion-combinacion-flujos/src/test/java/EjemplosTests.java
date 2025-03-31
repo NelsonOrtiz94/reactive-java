c9de9a52e66db84fcc7a285505012f81aa39882c
NelsonOrtiz94 <ortnn990@gmail.com>
2025-03-31 11:40:51 -0500

    Se plantean Tests para ver como se ejecuta un merge con 2 Flux

---
+++ curso-programacion-reactiva/transformacion-combinacion-flujos/src/test/java/EjemplosTests.java
@@ -0,0 +1,67 @@
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+public class EjemplosTests {
+
+    @Test
+    public void transformMap(){
+        List<String> listaNombres = Arrays.asList("google", "abc", "fb", "Stackoverflow");
+        Flux<String> nombresFlux = Flux.fromIterable(listaNombres)
+                .filter(nombre -> nombre.length() > 5)
+                .map(nombre -> nombre.toUpperCase())
+                .log();
+
+        StepVerifier.create(nombresFlux)
+                .expectNext("GOOGLE", "STACKOVERFLOW")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testTransformUsingFlatMap(){
+        List<String> listaNombres = Arrays.asList("google", "abc", "fb", "Stackoverflow");
+        Flux<String> nombresFlux = Flux.fromIterable(listaNombres)
+                .filter(nombre -> nombre.length() > 5)
+                .flatMap(nombre -> {
+                    return Mono.just(nombre.toUpperCase());
+                })
+                .log();
+
+        StepVerifier.create(nombresFlux)
+                .expectNext("GOOGLE", "STACKOVERFLOW")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testCombinarFlujosUsandoMerge(){
+        Flux<String> flux1 = Flux.just("Lucho", "Mo", "Julian");
+        Flux<String> flux2 = Flux.just("Diaz", "Salah", "Alvarez");
+
+        Flux<String> fluxMerge = Flux.merge(flux1, flux2).log(); //Combina múltiples observables
+
+        StepVerifier.create(fluxMerge)
+                .expectSubscription()
+                .expectNext("Lucho", "Mo", "Julian","Diaz", "Salah", "Alvarez")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testCombinarFlujosUsandoMergeConDelay(){
+        Flux<String> flux1 = Flux.just("Lucho", "Mo", "Julian")
+                .delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("Diaz", "Salah", "Alvarez")
+                .delayElements(Duration.ofSeconds(1));
+
+        Flux<String> fluxMerge = Flux.merge(flux1, flux2).log();
+
+        StepVerifier.create(fluxMerge)
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+    }
+}
