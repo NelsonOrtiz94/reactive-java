c00e18f5b14c68aae8025bd0b1d01c9374746887
NelsonOrtiz94 <ortnn990@gmail.com>
2025-04-14 11:18:24 -0500

    Se implementan el manejo de excepciones más especificamente .onErrorResume

---
+++ curso-programacion-reactiva/manejo.excepciones/src/main/java/Ejemplo02.java
@@ -0,0 +1,17 @@
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Ejemplo02 {
+    public static void main(String[] args) {
+
+        Flux.just(2,7,10)
+                .concatWith(Flux.error(new RuntimeException("Excepcion lanzada")))
+                .concatWith(Mono.just(12))
+                .onErrorResume(err -> {
+                    System.out.println("Error : " + err );
+                    return Mono.just(12);
+                })
+                .log()
+                .subscribe();
+    }
+}
