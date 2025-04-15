5395d98963e266c864710007bb45d2a35ec47a4d
NelsonOrtiz94 <ortnn990@gmail.com>
2025-04-15 15:59:38 -0500

    Se implementan el manejo de excepciones en otros ejercicios puntutales.

--- curso-programacion-reactiva/manejo.excepciones/src/main/java/Ejemplo04.java
+++ curso-programacion-reactiva/manejo.excepciones/src/main/java/Ejemplo04.java
@@ -1,17 +1,17 @@
 import reactor.core.publisher.Flux;
 
-public class Ejemplo03 {
+public class Ejemplo04 {
     public static void main(String[] args) {
 
         Flux.just(2,0,10, 8,12,22,24)
                 .map(element -> {
-                    if (element  == 0) {
+                    if (element  == 8) {
                         throw new RuntimeException("Excepcion lanzada");
                     }
                     return element;
-                }).onErrorContinue((ex, element) -> {
+                }).onErrorMap(ex -> {
                     System.out.println("Excepcion : " + ex);
-                    System.out.println("Elemento que genero el error es: " + element);
+                    return new CustomException(ex.getMessage(),ex);
                 })
                 .log()
                 .subscribe();
