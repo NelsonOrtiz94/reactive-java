69b7d8b0d8b404910200a9a18bb1fc0a86e04340
NelsonOrtiz94 <ortnn990@gmail.com>
2025-04-18 19:45:54 -0500

    Se estructura controlador reactivo

---
+++ curso-programacion-reactiva/ejemplo-thymeleaf-reactivo/ejemplo-thymeleaf-reactivo/src/main/java/com/ejemplo/reactivo/ControladorReactivo.java
@@ -0,0 +1,22 @@
+package com.ejemplo.reactivo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+@Controller
+public class ControladorReactivo {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @RequestMapping("/lista")
+    public String listarProductos(Model model) {
+        IReactiveDataDriverContextVariable listaReactiva = new ReactiveDataDriverContextVariable(productoRepository.buscarTodos(),1);
+        model.addAttribute("listarProdcutos", listaReactiva);
+        return "lista";
+    }
+}
--- curso-programacion-reactiva/ejemplo-thymeleaf-reactivo/ejemplo-thymeleaf-reactivo/src/main/java/com/ejemplo/reactivo/ProductoRepository.java
+++ curso-programacion-reactiva/ejemplo-thymeleaf-reactivo/ejemplo-thymeleaf-reactivo/src/main/java/com/ejemplo/reactivo/ProductoRepository.java
@@ -1,7 +1,9 @@
 package com.ejemplo.reactivo;
 
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,5 +16,9 @@ public class ProductoRepository {
         lista.add(new Producto(1, "ordenador", 100));
         lista.add(new Producto(2, "Diademas", 200));
         lista.add(new Producto(3, "Cascos", 300));
+    }
+
+    public Flux<Producto> buscarTodos() {
+        return Flux.fromIterable(lista).delayElements(Duration.ofSeconds(3));
     }
 }
