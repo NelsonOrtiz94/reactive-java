be1a7cbeaa593110bdf8f7bfa82d0b614813d195
NelsonOrtiz94 <ortnn990@gmail.com>
2025-06-23 22:24:25 -0500

    Se estructura Handler

---
+++ curso-programacion-reactiva/api.rest.reactiva-mongo/api.rest.reactiva-mongo/src/main/java/com/api/rest/reactiva/functional/ContactoHandler.java
@@ -0,0 +1,10 @@
+package com.api.rest.reactiva.functional;
+
+import com.api.rest.reactiva.repository.ContactoRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContactoHandler {
+
+    private ContactoRepository contactoRepository;
+}
