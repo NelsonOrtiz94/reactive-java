1d9ab7db2a20ca726274c15bbe71dc65a8ccd679
NelsonOrtiz94 <ortnn990@gmail.com>
2025-06-25 10:04:02 -0500

    Se estructura Test Repo para pruebas con Stepverifier y WebTestClient

---
+++ curso-programacion-reactiva/api.rest.reactiva-mongo/api.rest.reactiva-mongo/src/test/java/com/api/rest/reactiva/repository/ContactoRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.api.rest.reactiva.repository;
+
+import com.api.rest.reactiva.documents.Contacto;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ContactoRepositoryTest {
+
+    @Autowired
+    private ContactoRepository contactoRepository;
+
+    private ReactiveMongoOperations mongoOperations;
+
+    @BeforeAll
+    public void insertarDatos() {
+        Contacto contacto1 = new Contacto();
+        contacto1.setNombre("Test1");
+        contacto1.setEmail("c1@gmail.com");
+        contacto1.setTelefono("133222");
+
+        Contacto contacto2 = new Contacto();
+        contacto2.setNombre("Test2");
+        contacto2.setEmail("c2@gmail.com");
+        contacto2.setTelefono("233222");
+
+        Contacto contacto3 = new Contacto();
+        contacto3.setNombre("Test3");
+        contacto3.setEmail("c3@gmail.com");
+        contacto3.setTelefono("333222");
+    }
+}
